27822fe4fa91c069760fc33bac163958d89bf4ca
jeehyun <cjh7652@hanmail.net>
2025-07-09 10:22:44 +0900

    비동기

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,23 +3,41 @@ import axios from 'axios';
 
 const Home = () => {
    /*  546c72b99cf64514c2c03c7ef473011b */
+   const [upComingMovies, setUpComingMovies] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
 
    const getMovies = async () => {
         try{
             const response = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=546c72b99cf64514c2c03c7ef473011b&language=ko`);
-            console.log(response);
+            setUpComingMovies(response.data.results);
+            //console.log(response.data.results);
+            setIsLoading(false);
         }catch(error){
-            console.error(error)
+            console.error(error);
+            setIsLoading(false);
         }
    }
    useEffect(()=>{
          getMovies();
-   }, [])
+   }, []);
+
+   const getRandomMovie = () => {
+        if (upComingMovies.length === 0) return null;
+        const randomIndex = Math.floor(Math.random() * upComingMovies.length);
+        return upComingMovies[randomIndex];
+   }
+   const randomMovie = getRandomMovie();
     return (
         <div className='home'>
             <div className="upComing">
-                    
-            </div>
+                   {
+                    isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <p>{randomMovie ? randomMovie.title : "No upcoming movies"}</p>
+                    )
+                    }
+            </div>     
             <div className="search"></div>
 
         </div>
